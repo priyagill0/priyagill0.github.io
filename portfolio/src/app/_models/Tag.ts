f46deeddd85d08e7adc5ed7166bc2505ad2bf40b
Priya Gill <62481697+priyagill0@users.noreply.github.com>
2025-07-01 22:10:20 +0000

    addings changes

--- portfolio/src/app/_models/Tag.ts
+++ portfolio/src/app/_models/Tag.ts
@@ -7,6 +7,15 @@ export class Tag {
     static readonly MongoDB = new Tag('MongoDB', '#06402B');
     static readonly PostgreSQL = new Tag('PostgreSQL', '#90D5FF');
     static readonly MYSQL = new Tag('MySQL', '#3776ab');
+    static readonly PANDAS = new Tag('Pandas', '#150458');
+    static readonly NUMPY = new Tag('NumPy', '#150458');
+    static readonly SEABORN = new Tag('Seaborn', '#150458');
+    static readonly MATPLOTLIB = new Tag('Matplotlib', '#150458');
+    static readonly REACT = new Tag('React', '#150458');
+    static readonly SPRINGBOOT = new Tag('Spring Boot', '#150458');
+    static readonly OPENAI = new Tag('OpenAI', '#150458');
+    static readonly LANGCHAIN = new Tag('Langchain', '#150458');
+    static readonly FastAPI = new Tag('FastAPI', '#150458');
 
     private constructor (private readonly key: string, public readonly colour: string) {
 
