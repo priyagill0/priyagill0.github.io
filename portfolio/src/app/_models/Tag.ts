64081f2acba973dd089ec19956759c3aacc4a9f7
Priya Gill <priyagill@Priyas-MacBook-Pro.local>
2025-07-01 19:31:00 -0400

    Adding styling to tags

--- portfolio/src/app/_models/Tag.ts
+++ portfolio/src/app/_models/Tag.ts
@@ -1,22 +1,22 @@
 export class Tag {
-    static readonly Angular = new Tag('Angular', '#950606');
-    static readonly Python = new Tag('Python', '#551a8b');
-    static readonly Typescript = new Tag('Typescript', '#3776ab');
-    static readonly Java = new Tag('Java', '#ED8B00');
-    static readonly SQL = new Tag('SQL', '#008080');
-    static readonly MongoDB = new Tag('MongoDB', '#06402B');
+    static readonly Angular = new Tag('Angular', '#ff746c');
+    static readonly Python = new Tag('Python', '#75b5f5');
+    static readonly Typescript = new Tag('Typescript', '#3271a5');
+    static readonly Java = new Tag('Java', '#ffc067');
+    static readonly SQL = new Tag('SQL', '#99c5c4');
+    static readonly MongoDB = new Tag('MongoDB', '#b2d4b2');
     static readonly PostgreSQL = new Tag('PostgreSQL', '#90D5FF');
-    static readonly MYSQL = new Tag('MySQL', '#3776ab');
-    static readonly PANDAS = new Tag('Pandas', '#150458');
-    static readonly NUMPY = new Tag('NumPy', '#150458');
-    static readonly SEABORN = new Tag('Seaborn', '#150458');
-    static readonly MATPLOTLIB = new Tag('Matplotlib', '#150458');
-    static readonly REACT = new Tag('React', '#150458');
-    static readonly SPRINGBOOT = new Tag('Spring Boot', '#150458');
-    static readonly OPENAI = new Tag('OpenAI', '#150458');
-    static readonly LANGCHAIN = new Tag('Langchain', '#150458');
-    static readonly FastAPI = new Tag('FastAPI', '#150458');
-
+    static readonly MYSQL = new Tag('MySQL', '#c9a0dc');
+    static readonly PANDAS = new Tag('Pandas', '#ffc5b3');
+    static readonly NUMPY = new Tag('NumPy', '#f988d9');
+    static readonly SEABORN = new Tag('Seaborn', '#d3b392');
+    static readonly MATPLOTLIB = new Tag('Matplotlib', '#3eb489');
+    static readonly REACT = new Tag('React', '#90d5ff');
+    static readonly SPRINGBOOT = new Tag('Spring Boot', '#85cf3b');
+    static readonly OPENAI = new Tag('OpenAI', '#ccccff');
+    static readonly LANGCHAIN = new Tag('Langchain', '#f5c5ff');
+    static readonly FastAPI = new Tag('FastAPI', '#74d1c4');
+    static readonly JAVASCRIPT = new Tag('JavaScript', '#f7df1e');
     private constructor (private readonly key: string, public readonly colour: string) {
 
     }
