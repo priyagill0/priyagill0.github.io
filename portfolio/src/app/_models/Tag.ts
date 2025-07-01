3794dfcb1efda080063d4096bd4e033d41d66114
Priya Gill <62481697+priyagill0@users.noreply.github.com>
2025-07-01 18:21:36 +0000

    adding database project

--- portfolio/src/app/_models/Tag.ts
+++ portfolio/src/app/_models/Tag.ts
@@ -3,9 +3,10 @@ export class Tag {
     static readonly Python = new Tag('Python', '#551a8b');
     static readonly Typescript = new Tag('Typescript', '#3776ab');
     static readonly Java = new Tag('Java', '#ED8B00');
-    static readonly SQL = new Tag('SQL', '#90D5FF');
+    static readonly SQL = new Tag('SQL', '#008080');
     static readonly MongoDB = new Tag('MongoDB', '#06402B');
     static readonly PostgreSQL = new Tag('PostgreSQL', '#90D5FF');
+    static readonly MYSQL = new Tag('MySQL', '#3776ab');
 
     private constructor (private readonly key: string, public readonly colour: string) {
 
