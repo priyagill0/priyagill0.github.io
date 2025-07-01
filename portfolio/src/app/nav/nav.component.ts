7476023375d2789598015576adbab14b3dac527c
Priya Gill <62481697+priyagill0@users.noreply.github.com>
2025-07-01 05:55:18 +0000

    adding nav component imports

--- portfolio/src/app/nav/nav.component.ts
+++ portfolio/src/app/nav/nav.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { ResumeComponent } from '../resume/resume.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { ContactComponent } from '../contact/contact.component';
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule],
+  imports: [RouterModule, HomeComponent, ResumeComponent, PortfolioComponent, ContactComponent, CommonModule],
   standalone: true,
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
