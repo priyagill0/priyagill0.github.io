d24da4d3ca3242881e0ddbb8a3fb445f1141cba8
Priya Gill <62481697+priyagill0@users.noreply.github.com>
2025-07-01 18:21:15 +0000

    adding more skills and testing animation

--- portfolio/src/app/resume/resume.component.ts
+++ portfolio/src/app/resume/resume.component.ts
@@ -13,3 +13,38 @@ export class ResumeComponent {
   }
 }
 
+// import { AfterViewInit, Component } from '@angular/core';
+// import { Title } from '@angular/platform-browser';
+
+// @Component({
+//   selector: 'app-resume',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './resume.component.html',
+//   styleUrl: './resume.component.scss'
+// })
+// export class ResumeComponent implements AfterViewInit {
+//   constructor(private titleService: Title) {
+//     this.titleService.setTitle('Priya Gill - Resume');
+//   }
+
+//   ngAfterViewInit(): void {
+//     const skillsSection = document.querySelector('#skills');
+//     const skillItems = document.querySelectorAll('.skill-item');
+
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           skillItems.forEach(item => item.classList.add('visible'));
+//           observer.unobserve(entry.target);  // Only trigger once
+//         }
+//       });
+//     }, {
+//       threshold: 0.2
+//     });
+
+//     if (skillsSection) {
+//       observer.observe(skillsSection);
+//     }
+//   }
+// }
