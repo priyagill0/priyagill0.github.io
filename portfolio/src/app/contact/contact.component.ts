ea6a3304f4ee01a108100b925c71940b88ff5374
Priya Gill <priyagill@Priyas-MacBook-Pro.local>
2025-09-08 19:03:34 -0400

    adding icon for website

--- portfolio/src/app/contact/contact.component.ts
+++ portfolio/src/app/contact/contact.component.ts
@@ -9,6 +9,6 @@ import { Title } from '@angular/platform-browser';
 })
 export class ContactComponent {
   constructor(private titleService: Title) {
-      this.titleService.setTitle('Priya Gill - Contact');
+      this.titleService.setTitle('Priya Gill');
     }
 }
